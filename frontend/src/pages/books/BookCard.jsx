e76d0098b410b351ed011759ee9c8a797fddbf2d
BARRY <barryaliouhynde@gmail.com>
2026-06-02 20:19:11 +0100

    Second Day

---
+++ frontend/src/pages/books/BookCard.jsx
@@ -0,0 +1,54 @@
+import { FiShoppingCart } from 'react-icons/fi'
+import { getImgUrl } from '../../utils/getImgUrl'
+import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../../redux/features/cart/cartSlice';
+
+
+const BookCard = ({ book }) => {
+
+    const dispatch =  useDispatch();
+
+    const handleAddToCart = (product) => {
+        dispatch(addToCart(product))
+    }
+
+    return (
+    <div className=" rounded-lg transition-shadow duration-300">
+        <div
+                className="flex flex-col sm:flex-row sm:items-center sm:h-72  sm:justify-center gap-2"
+>
+            <div className="sm:h-72 sm:flex-shrink-0 border rounded-md">
+                <Link to={`/books/${book._id}`}>
+                    <img
+                    src={getImgUrl(book?.coverImage)}
+                    alt=""
+                    className="w-full bg-cover p-2 rounded-md cursor-pointer hover:scale-105 transition-all duration-200"
+                    />
+                </Link>
+            </div>
+
+            <div>
+                <Link to={`/books/${book?._id}`}>
+                    <h3 className="text-lg font-semibold hover:text-blue-600 mb-3">
+                        {book?.title}
+                    </h3>
+                </Link>
+                <p className="text-gray-600 mb-5">{book?.description.length > 80 ? book?.description.slice(0, 80) + '...' : book?.description}</p>
+                <p className="font-medium mb-5">
+                    ${book?.newPrice} <span className="line-through font-normal ml-2">${book?.oldPrice}</span>
+                </p>
+                <button 
+                    onClick={() => handleAddToCart(book)}
+                    className="flex items-center gap-1 bg-primary px-4 py-3 rounded-md text-sm font-secondary font-bold hover:bg-secondary hover:text-white transition-all duration-200 cursor-pointer">
+                    <FiShoppingCart />
+                    <span>Add to Cart</span>
+                </button>
+            </div>
+        </div>
+    </div>
+
+    )
+}
+
+export default BookCard
