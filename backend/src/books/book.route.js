d49cb853ddca61f4511e7cc6abf4b857f3177f84
BARRY <barryaliouhynde@gmail.com>
2026-06-04 20:06:56 +0100

    Third Day

---
+++ backend/src/books/book.route.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const Book = require('./book.model');
+const { postABook, getAllBooks,getSingleBook,UpdateBook,deleteABook } = require('./book.controller');
+const verifyAdminToken = require('../middleware/verifyAdminToken');
+
+
+const router = express.Router();
+
+// post a book
+router.post("/create-book", verifyAdminToken, postABook);
+
+// get all books
+router.get("/", getAllBooks);
+
+// get a single book endpoint
+router.get("/:id", getSingleBook);
+
+// update a book endpoint
+router.put("/edit/:id", verifyAdminToken, UpdateBook);
+
+// delete a book endpoint
+router.delete("/:id", verifyAdminToken, deleteABook)
+
+
+module.exports = router;
